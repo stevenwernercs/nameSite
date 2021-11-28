b3b3aaf5081faff082744e049010ffe3788d1935
Steven Werner <stevenWernerCS@gmail.com>
2021-11-28 11:06:46 -0600

    adding map

--- about.php
+++ about.php
@@ -3,6 +3,34 @@ $pageTitle="about:Werner";
 $pageName="about";
 require_once("header.inc");
 ?>
+<style>
+#map {
+ height: 560px;
+ width: 100%;
+ overflow: hidden;
+ float: left;
+ border: thin solid #333;
+ }
+#overview {
+  position: absolute;
+  left: 40px;
+  height: 175px;
+  width: 175px;
+  bottom: 50px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+}
+#capture {
+ height: 100%;
+ width: 100%;
+ overflow: hidden;
+ float: left;
+ background-color: #ECECFB;
+ border: thin solid #333;
+ border-top: none;
+ }
+.selected { font-weight: bold; }
+</style>
+
 
  <!-- static Carousel
     ================================================== -->
@@ -75,13 +103,154 @@ require_once("header.inc");
 	<div class="col-lg-4">
 	  <a href="https://www.rtx.com" title="Raytheon Technologies: Intellegence Information Services" target="_blank"><img class="img-circle" src="img/square/rtx.png" style="width: 140px; height: 140px;"></a>
           <h3>Data Science</h3>
-	  <p>Sr Software Engineer I at a leading Intellegence Contractor. Specializations in NLP, ML & Data Science</p>
+	  <p>Sr Software Engineer at a leading Intellegence Contractor. Specializations in NLP, ML & Data Science</p>
           <!-- <p><a class="btn btn-default" href="http://getbootstrap.com/examples/carousel/#" role="button">View details »</a></p> -->
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
-
+     </div>
+ 
       <hr class="featurette-divider">
-
+      
+      <div class="row">
+	<div class="col-lg-2">
+		<div id="toggle_box" style="position: absolute; top: 100px; right: 20px; padding: 10px; background: #fff; z-index: 5; "></div>
+	</div>
+	<div class="col-lg-8">
+		<div id="map"></div>
+		<div id="overview"></div>
+	</div>
+      </div>
+      <div class="row">
+	<div class="col-lg-2">
+	</div>
+	<div class="col-lg-8">
+		<div id="capture"></div>
+	</div>
+	<script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCG86U3_subBCpgXS4UOHWCi9nHzZ5Xhcc&callback=initMap">
+	</script>
+	</div>
+      </div>
+	
+      <!-- /.row -->
       <!-- /END THE FEATURETTES -->
 
+<script>
+	let map, overview;
+	const OVERVIEW_DIFFERENCE = 5;
+	const OVERVIEW_MIN_ZOOM = 3;
+	const OVERVIEW_MAX_ZOOM = 10;
+
+	// lets define some vars to make things easier later
+	var kml = {
+	    a: {
+	        name: "Life: Everywhere I spent more than a week at",
+		url: "https://stevenwernercs.com/documents/life-tmp.kml"
+	    },
+	    b: {
+	        name: "Wiki: Events since 1985",
+		url: "https://stevenwernercs.com/documents/wiki-800.kml"
+	    }
+	};
+
+	function initMap() {
+	  const mapOptions = {
+	    center: { lat: 50, lng: 8 },
+	    zoom: 7,
+	  };	
+	  
+	  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	  
+	  createTogglers();
+
+	  // instantiate the overview map without controls
+	  overview = new google.maps.Map(document.getElementById("overview"), {
+	    ...mapOptions,
+	    disableDefaultUI: true,
+	    gestureHandling: "none",
+	    zoomControl: false,
+	  });
+
+	  function clamp(num, min, max) {
+	    return Math.min(Math.max(num, min), max);
+	  };
+
+	  map.addListener("bounds_changed", () => {
+	    overview.setCenter(map.getCenter());
+	    overview.setZoom(
+	      clamp(
+	        map.getZoom() - OVERVIEW_DIFFERENCE,
+	        OVERVIEW_MIN_ZOOM,
+	        OVERVIEW_MAX_ZOOM
+	      )
+	    );
+	  });
+	};
+
+	//google.maps.event.addDomListener(window, 'load', initMap);
+
+	// the important function... kml[id].xxxxx refers back to the top 
+	function toggleKML(checked, id) {
+	    if (checked) {
+
+        	var layer = new google.maps.KmlLayer(kml[id].url, {
+	            preserveViewport: true,
+        	    suppressInfoWindows: true 
+	        });
+	 	layer.addListener('click', function(event) {
+		    var content = event.featureData.infoWindowHtml;
+		    var testimonial = document.getElementById('capture');
+		    testimonial.innerHTML = content;
+		});
+        	// store kml as obj
+	        kml[id].obj = layer;
+        	kml[id].obj.setMap(map);
+	    }
+	    else {
+	        kml[id].obj.setMap(null);
+        	delete kml[id].obj;
+	    }
+	};
+
+	// create the controls dynamically because it's easier, really
+	function createTogglers() {
+	    var html = "<form><ul>";
+	    for (var prop in kml) {
+        	html += "<li id=\"selector-" + prop + "\"><input type='checkbox' id='" + prop + "'" +
+	        " onclick='highlight(this,\"selector-" + prop + "\"); toggleKML(this.checked, this.id)' \/>" +
+	        "&nbsp" + kml[prop].name + "<\/li>";
+	    }
+	    html += "<li class='control'><a href='#' onclick='removeAll();return false;'>" +
+	    "Remove all layers<\/a><\/li>" + 
+	    "<\/ul><\/form>";
+	
+	    document.getElementById("toggle_box").innerHTML = html;
+	};
+	
+	// easy way to remove all objects
+	function removeAll() {
+	    for (var prop in kml) {
+	        if (kml[prop].obj) {
+	            kml[prop].obj.setMap(null);
+	            delete kml[prop].obj;
+	        }
+	    	document.getElementById(prop).checked = false;
+	    }
+	};
+	
+	// Append Class on Select
+	function highlight(box, listitem) {
+	    var selected = 'selected';
+	    var normal = 'normal';
+	    document.getElementById(listitem).className = (box.checked ? selected: normal);
+	};
+
+	function startup() { 
+		// for example, this toggles kml b on load and updates the menu selector
+		var checkit = document.getElementById('b');
+		checkit.checked = true;
+		toggleKML(checkit, 'b');
+		highlight(checkit, 'selector1');
+	 }
+		
+</script>
 <?php include_once("footer.inc"); ?> 
