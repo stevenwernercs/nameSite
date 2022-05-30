34691e2d54fa57ccce5be94a497205b9ee292f83
Steven Werner <stevenWernerCS@gmail.com>
2022-05-30 16:27:46 -0500

    adding resume update to script

--- package.sh
+++ package.sh
@@ -3,4 +3,10 @@
 
 #find | egrep -v "(/\.git|\.swp|~$|${0})" 
 
+
+if [ -e "../resume/Werner-resume.pdf" ]
+then
+  cp "../resume/Werner-resume.pdf" ./documents/
+fi
+
 rsync -auvz --exclude-from package-exclude.ctl . "${1}"
