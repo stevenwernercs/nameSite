ec810fa4459e36972be913b30c11756743358151
Steven Werner <stevenWernerCS@gmail.com>
2022-06-29 19:35:36 -0500

    updating resume

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@
 
 if [ -e "../resume/Werner-resume.pdf" ]
 then
-  cp "../resume/Werner-resume.pdf" ./documents/
+  cp "../resume/Werner-resume.pdf" ./documents/Werner-Resume.pdf
 fi
 
 rsync -auvz --exclude-from package-exclude.ctl . "${1}"
